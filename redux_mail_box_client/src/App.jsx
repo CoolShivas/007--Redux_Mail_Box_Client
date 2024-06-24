78c2cfe58a1c4e1b049a93267d005f945ded61c2
CoolShivas <shivas2710cool00@gmail.com>
2024-06-24 11:38:19 +0530

    Starting of Rdx MailBox Client

---
+++ redux_mail_box_client/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (<>
+    <h1> Redux Mail Box Client Project. </h1>
+    <h2> Going to have fun and frustration by doing this. </h2>
+    <h3> Lets start with me... </h3>
+  </>)
+};
+
+export default App;
