f219282ac9530f346608389616a66964a7ef3370
CoolShivas <shivas2710cool00@gmail.com>
2024-06-24 11:51:47 +0530

    Installation of Tailwind CSS and uses.

--- redux_mail_box_client/src/App.jsx
+++ redux_mail_box_client/src/App.jsx
@@ -1,8 +1,8 @@
 function App() {
   return (<>
-    <h1> Redux Mail Box Client Project. </h1>
-    <h2> Going to have fun and frustration by doing this. </h2>
-    <h3> Lets start with me... </h3>
+    <h1 className="text-3xl font-bold bg-red-300"> Redux Mail Box Client Project. </h1>
+    <h2 className="text-[#4c1d95] text-2xl p-2"> Going to have fun and frustration by doing this. </h2>
+    <h3 className="text-2xl text-red-500 font-bold underline m-5"> Lets start with me... </h3>
   </>)
 };
 
