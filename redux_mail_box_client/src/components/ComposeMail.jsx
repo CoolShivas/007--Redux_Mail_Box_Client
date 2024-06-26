30efba0dbdd2303203790604d1757ed3c497b840
CoolShivas <shivas2710cool00@gmail.com>
2024-06-26 11:33:33 +0530

    Instead of TextArea using ReactQuill Rich Text Editor. And Two-Way_Binding or Stitching of data and getting on Console.

--- redux_mail_box_client/src/components/ComposeMail.jsx
+++ redux_mail_box_client/src/components/ComposeMail.jsx
@@ -1,10 +1,30 @@
+import "react-quill/dist/quill.snow.css";
+import ReactQuill from "react-quill";
+import { NavLink } from "react-router-dom/cjs/react-router-dom.min";
+import { useState } from "react";
 
 
 const ComposeMail = () => {
+
+    const [to, setTo] = useState("");
+    const [subject, setSubject] = useState("");
+    const [contentBox, setContentBox] = useState("");
+
+    const handlerOnSubmitCompose = (e) => {
+        e.preventDefault();
+
+        const sendComposeData = {
+            Email: to,
+            Subject: subject,
+            ContentBox: contentBox,
+        };
+        console.log(sendComposeData);
+    };
+
     return (
         <div>
             <div className="m-5 p-4 w-5/6 float-right bg-[#8b5cf6] rounded-2xl">
-                <form>
+                <form onSubmit={handlerOnSubmitCompose}>
 
                     <div className="p-4 w-full">
                         <input
@@ -13,6 +33,8 @@ const ComposeMail = () => {
                             placeholder="To:"
                             className="w-full border-b-2 p-3 border-purple-400 rounded-xl"
                             required
+                            value={to}
+                            onChange={(e) => setTo(e.target.value)}
                         />
                     </div>
 
@@ -22,21 +44,30 @@ const ComposeMail = () => {
                             type="text"
                             placeholder="Subject Matter"
                             className="w-full border-b-2  border-purple-400 p-3 rounded-xl"
+                            value={subject}
+                            onChange={(e) => setSubject(e.target.value)}
                         />
                     </div>
 
                     <div className=" p-4">
-                        <textarea className="w-full h-full" />
+                        {/* <textarea className="w-full h-full" /> */}
+                        <ReactQuill
+                            id="editor"
+                            value={contentBox}
+                            onChange={setContentBox}>
+                        </ReactQuill>
                     </div>
 
                     <div className=" p-4 text-center  ">
-                        <button className="rounded-2xl p-4 bg-[#86efac] font-bold  hover:bg-sky-700 hover:text-white w-[10%] float-left">
+                        <button className="rounded-2xl p-4 bg-[#86efac] font-bold  hover:bg-sky-700 hover:text-white w-[10%] float-left" type="submit">
                             Send
                         </button>
 
-                        <button className="rounded-2xl font-bold p-4 bg-[#fecaca] font-bold hover:text-white hover:bg-[#dc2626] w-[10%] float-right">
-                            Cancel
-                        </button>
+                        <NavLink to="/mainpage" >
+                            <button className="rounded-2xl font-bold p-4 bg-[#fecaca] font-bold hover:text-white hover:bg-[#dc2626] w-[10%] float-right">
+                                Cancel
+                            </button>
+                        </NavLink>
                     </div>
 
                 </form>
