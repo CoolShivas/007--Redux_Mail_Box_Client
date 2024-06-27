5e87c360a0af890dfc45d80587188546caa0f799
CoolShivas <shivas2710cool00@gmail.com>
2024-06-27 14:50:52 +0530

    Completion of ReadInbox and ReadSentbox with closing btn and rendering  of data to its appropriate places with the  help of useParams Hook using find method to compare same message with same id.

--- redux_mail_box_client/src/components/ReadInbox.jsx
+++ redux_mail_box_client/src/components/ReadInbox.jsx
@@ -1,19 +1,29 @@
+import { NavLink } from "react-router-dom/cjs/react-router-dom.min";
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom/cjs/react-router-dom.min";
 
 
 const ReadInbox = () => {
+
+    const { emailId } = useParams();
+
+    const incoming = useSelector((store) => store.electronicMails.mails.find((mail) => mail.id === emailId));
+
     return (
         <div>
             <div className="flex justify-center h-96 p-2">
                 <div className=" bg-emerald-200 shadow-lg rounded-lg p-6 m-4 w-full max-w-2xl">
-                    <button className="font-bold bg-red-400 h-6 w-6 text-white text-center float-right hover:h-8 hover:w-8 hover:bg-red-600 hover:rounded"> X </button>
+                    <NavLink to="/mainpage/inbox">
+                        <button className="font-bold bg-red-400 h-6 w-6 text-white text-center float-right hover:h-8 hover:w-8 hover:bg-red-600 hover:rounded"> X </button>
+                    </NavLink>
                     <h1 className="text-2xl font-bold mb-4">
-                        From:
+                        From: {incoming.to}
                     </h1>
                     <h2 className="text-xl font-semibold mb-2">
-                        Subject:
+                        Subject: {incoming.subject}
                     </h2>
                     <h4 className="text-gray-700 mb-4 text-sm">Message:</h4>
-                    <p className="bg-gray-200 p-4 rounded-lg h-auto"> Hello World </p>
+                    <p className="bg-gray-200 p-4 rounded-lg h-auto"> {incoming.contentBox} </p>
                 </div>
             </div>
         </div>
--- redux_mail_box_client/src/components/ReadSentbox.jsx
+++ redux_mail_box_client/src/components/ReadSentbox.jsx
@@ -1,19 +1,29 @@
+import { NavLink } from "react-router-dom/cjs/react-router-dom.min";
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom/cjs/react-router-dom.min";
 
 
 const ReadSentbox = () => {
+
+    const { emailId } = useParams();
+
+    const outgoing = useSelector((store) => store.electronicMails.sents.find((mail) => mail.id === emailId));
+
     return (
         <div>
             <div className="flex justify-center h-96 p-2">
                 <div className=" bg-emerald-200 shadow-lg rounded-lg p-6 m-4 w-full max-w-2xl">
-                    <button className="font-bold bg-red-400 h-6 w-6 text-white text-center float-right hover:h-8 hover:w-8 hover:bg-red-600 hover:rounded"> X </button>
+                    <NavLink to="/mainpage/sentbox">
+                        <button className="font-bold bg-red-400 h-6 w-6 text-white text-center float-right hover:h-8 hover:w-8 hover:bg-red-600 hover:rounded"> X </button>
+                    </NavLink>
                     <h1 className="text-2xl font-bold mb-4">
-                        To:
+                        To: {outgoing.to}
                     </h1>
                     <h2 className="text-xl font-semibold mb-2">
-                        Subject:
+                        Subject: {outgoing.subject}
                     </h2>
                     <h3 className="text-gray-700 mb-4 text-sm">Message:</h3>
-                    <p className="bg-gray-200 p-4 rounded-lg h-auto"> Hello World </p>
+                    <p className="bg-gray-200 p-4 rounded-lg h-auto"> {outgoing.contentBox} </p>
                 </div>
             </div>
         </div>
